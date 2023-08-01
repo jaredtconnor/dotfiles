4d5c79d9efd1fe96f3db66d95d6ef6e5ddc8252a
Jared Connor <jaredconnor@fastmail.com>
2023-07-31 19:18:35 -0700

    Adding neovim and zsh config using stow

---
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -0,0 +1,7 @@
+vim.g.dap_virtual_text = true
+
+-- Disable persistent undo for files in /private directory
+vim.api.nvim_create_autocmd("BufReadPre", { pattern = "/private/*", command = "set noundofile" })
+
+-- Enable persistent undo for other files
+vim.opt.undofile = true
