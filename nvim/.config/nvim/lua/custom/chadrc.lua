6abb9117f8c4e59cdfe9cdab2c3e67a160d55a58
Jared Connor <jaredconnor301@gmail.com>
2023-08-14 20:46:00 -0700

    Update to nvim and git

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -1,6 +1,4 @@
 ---@type ChadrcConfig 
-local M = {}
-M.ui = {theme = 'tokyonight'}
-M.plugins = "custom.plugins"
-M.mappings = require "custom.mappings"
-return M
+ local M = {}
+ M.ui = {theme = 'onedark'}
+ return M
