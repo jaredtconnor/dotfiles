f5ab9ec2aafd48fd645053c3222c877855bfd88e
Jared Connor <jaredconnor@fastmail.com>
2023-09-22 16:51:25 -0700

    Adding typescript setup 1

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,8 +1,22 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
+local config = require("plugins.configs.lspconfig")
 
 local lspconfig = require("lspconfig")
 local util = require("lspconfig/util")
+
+local function organize_imports()
+  local params {
+    command = "_typescript.organizeImports",
+    arguments = {vim.api.nvim_buf_get_name(0)},
+  } 
+  
+  vim.lsp.buf.execute_command(params)
+  
+end
+
+
+
 
 lspconfig.pyright.setup({
 	on_attach = on_attach,
