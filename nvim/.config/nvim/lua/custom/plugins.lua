05e229ef49a21e859ce231e0df4f4791c0cf9a1b
Jared Connor <jaredconnor301@gmail.com>
2023-12-05 15:12:41 -0800

    Moving back to Nvchad

---
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,181 @@
+local overrides = require("custom.configs.overrides")
+
+local plugins = {
+
+	-- override default plugins
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
+
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
+
+	-- lsp settings
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		ft = "go",
+		opts = function()
+			return require("custom.configs.null-ls")
+		end,
+	},
+
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
+	{
+		"simrat39/rust-tools.nvim",
+		ft = "rust",
+		dependencies = "neovim/nvim-lspconfig",
+		opts = function()
+			return require("custom.configs.rust-tools")
+		end,
+		config = function(_, opts)
+			require("rust-tools").setup(opts)
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		init = function()
+			require("core.utils").load_mappings("dap")
+		end,
+	},
+	{
+		"dreamsofcode-io/nvim-dap-go",
+		ft = "go",
+		dependencies = "mfussenegger/nvim-dap",
+		config = function(_, opts)
+			require("dap-go").setup(opts)
+			require("core.utils").load_mappings("dap_go")
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap-python",
+		ft = "python",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"rcarriga/nvim-dap-ui",
+		},
+		config = function(_, opts)
+			local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+			require("dap-python").setup(path)
+			require("core.utils").load_mappings("dap_python")
+		end,
+	},
+	{
+		"olexsmir/gopher.nvim",
+		ft = "go",
+		config = function(_, opts)
+			require("gopher").setup(opts)
+		end,
+		build = function()
+			vim.cmd([[silent! GoInstallDeps]])
+		end,
+	},
+	{
+		"saecki/crates.nvim",
+		ft = { "rust", "toml" },
+		config = function(_, opts)
+			local crates = require("crates")
+			crates.setup(opts)
+			require("cmp").setup.buffer({
+				sources = { { name = "crates" } },
+			})
+			crates.show()
+			require("core.utils").load_mappings("crates")
+		end,
+	},
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
+	{
+		"theHamsta/nvim-dap-virtual-text",
+		lazy = false,
+		config = function(_, opts)
+			require("nvim-dap-virtual-text").setup()
+		end,
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		lazy = false,
+		opts = function()
+			return require("custom.configs.copilot")
+		end,
+		config = function(_, opts)
+			require("copilot").setup(opts)
+		end,
+	},
+	{
+		"anuvyklack/pretty-fold.nvim",
+		lazy = false,
+		config = function()
+			require("pretty-fold").setup()
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+	},
+	{
+		"akinsho/git-conflict.nvim",
+		version = "*",
+		config = true,
+	},
+	{
+		"ThePrimeagen/harpoon",
+		name = "harpoon",
+	},
+	{
+		"jackMort/ChatGPT.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("chatgpt").setup({
+				-- keymaps = {
+				-- submit = "<C-s>"
+				-- }
+			})
+		end,
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+	"Bryley/neoai.nvim",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
+}
+
+return plugins
