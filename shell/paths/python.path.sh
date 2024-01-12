e664f18ee311676fa92c7347bda79997d4cda34b
Jared Connor <jaredconnor@fastmail.com>
2024-01-12 07:26:49 -0800

    Adding python path

--- shell/paths/python.path.sh
+++ shell/paths/python.path.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Use homebrew python links
-export PATH=$PATH:/opt/homebrew/opt/python@3.11/libexec/bin
+export PATH=$PATH:/opt/homebrew/opt/python@3.11/libexec/bin 
+export PATH="$HOME/.local/bin:$PATH"
