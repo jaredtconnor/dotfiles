2d0457979321e1e6edf51c37f1d8c03a53a0a683
Jared Connor <jaredconnor301@gmail.com>
2024-01-20 08:00:28 -0800

    Update to theme

--- terminal/wezterm/wezterm.lua
+++ terminal/wezterm/wezterm.lua
@@ -1,9 +1,13 @@
 local wezterm = require("wezterm")
-return {
-  color_scheme = "Dracula",
+
+local config = {
   font = wezterm.font("JetBrainsMono Nerd Font"),
   font_size = 12.0,
   leader = { key = "a", mods = "CTRL" },
   hide_tab_bar_if_only_one_tab = true,
   keys = {},
 }
+
+config.color_scheme = "tokyonight-storm"
+
+return config
