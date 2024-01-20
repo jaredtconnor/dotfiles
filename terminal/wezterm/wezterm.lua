34196ed8b5ca78aa683ffb6e63e9915cd9c7bd2f
Jared Connor <jaredconnor301@gmail.com>
2024-01-19 21:07:26 -0800

    Moving to simple wezterm setup

--- terminal/wezterm/wezterm.lua
+++ terminal/wezterm/wezterm.lua
@@ -1,45 +1,19 @@
+-- Pull in the wezterm API
 local wezterm = require("wezterm")
-local utils = require("utils")
-local assets = wezterm.config_dir .. "/assets"
 
-local config = {
-  -- window_background_opacity = 0.15,
-  macos_window_background_blur = 30,
-  enable_tab_bar = false,
-  window_decorations = "RESIZE",
-  window_close_confirmation = "NeverPrompt",
-  font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" }),
-  font_rules = {
-    {
-      italic = true,
-      font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" }),
-    },
-  },
-  harfbuzz_features = { "calt", "dlig", "clig=1", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
-  font_size = 16,
-  line_height = 1.0,
-  adjust_window_size_when_changing_font_size = true,
-  native_macos_fullscreen_mode = true,
-  keys = {
-    {
-      key = "n",
-      mods = "SHIFT|CTRL",
-      action = wezterm.action.ToggleFullScreen,
-    },
-  },
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
-  send_composed_key_when_left_alt_is_pressed = true,
-  send_composed_key_when_right_alt_is_pressed = false,
-}
+-- This table will hold the configuration.
+local config = {}
 
-local is_dark = wezterm.gui.get_appearance():find("Dark")
-local theme = utils.get_current_theme()
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
 
-utils.table_extend(true, config, require(theme)(is_dark, assets))
+-- This is where you actually apply your config choices
 
+-- For example, changing the color scheme:
+-- wezterm.color.load_scheme("./tokyonight_storm.toml")
+
+-- and finally, return the configuration to wezterm
 return config
