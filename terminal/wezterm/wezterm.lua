a1773c9ec8641774b85301d8d0f112bcee7dc7a2
Jared Connor <jaredconnor301@gmail.com>
2024-02-24 06:35:18 -0800

    Updating wez term and asdf yarn version
    Driven by the yarn v1.222.21 missing some downloadable assets.

--- terminal/wezterm/wezterm.lua
+++ terminal/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = {
   window_decorations = "RESIZE",
-  font = wezterm.font("JetBrainsMono Nerd Font"),
+  font = wezterm.font("JetBrains Mono"),
   font_size = 12.0,
   leader = { key = "a", mods = "CTRL" },
   hide_tab_bar_if_only_one_tab = true,
