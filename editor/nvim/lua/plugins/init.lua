2cf7e2d6f87a04b1093199ceb0f10b907880ba15
Jared Connor <jaredconnor@fastmail.com>
2024-03-21 23:07:53 -0700

    Update to nvim

--- editor/nvim/lua/plugins/init.lua
+++ editor/nvim/lua/plugins/init.lua
@@ -1,8 +1,39 @@
--- NOTE: Default Plugins
+local overrides = require("configs.overrides")
+
 return {
-  -- NvChad Default Terminal
   {
-    "NvChad/nvterm",
-    enabled = false,
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre' -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua",
+  			"html-lsp", "css-lsp" , "prettier"
+  		},
+  	},
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
   },
 }
--- editor/nvim/lua/plugins/markdown-preview.lua
+++
@@ -1,8 +0,0 @@
--- NOTE: Preview Markdown
-return {
-  "iamcco/markdown-preview.nvim",
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
-  ft = "markdown",
-}
