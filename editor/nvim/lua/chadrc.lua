b6b33dbcda8659dc9bcc9b0c2940c35024939d75
Jared Connor <jaredconnor@fastmail.com>
2024-03-22 07:34:04 -0700

    Moving to cool setup

--- editor/nvim/lua/chadrc.lua
+++ editor/nvim/lua/chadrc.lua
@@ -1,13 +1,58 @@
----@type ChadrcConfig
 local M = {}
 
-M.ui = {
-	theme = "onedark",
+local highlights = require("highlights")
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+M.ui = {
+	theme = "tokyonight",
+	hl_override = highlights.override,
+
+	-- To fit with nvim-tree
+	tabufline = {
+		order = { "buffers", "tabs", "btns", "treeOffset" }, -- Add "treeOffset" if you dont use nvimTreee like a popup
+	},
+
+	term = {
+		hl = "Normal:term,WinSeparator:WinSeparator",
+		sizes = { sp = 0.3, vsp = 0.2 },
+		float = {
+			relative = "editor",
+			row = 0.1,
+			col = 0.045,
+			width = 0.9,
+			height = 0.8,
+			border = "single",
+		},
+	},
+}
+
+M.plugins = "plugins"
+
+M.ui.nvdash = {
+	load_on_startup = true,
+
+	header = {
+		"           ",
+		"           ",
+		"           ",
+		"           ",
+		"           ",
+		"  ⟋|､      ",
+		" (°､ ｡ 7   ",
+		" |､  ~ヽ   ",
+		" じしf_,)〳",
+	},
+
+	buttons = {
+		{ "  Projects", "", "Telescope projects" },
+		{ "  Themes  ", "", "Telescope themes" },
+		{ "  Mappings", "", "NvCheatsheet" },
+	},
+}
+
+M.disabled = {
+	n = {
+		["<leader>e"] = "",
+	},
 }
 
 return M
--- editor/nvim/lua/configs/conform.lua
+++ editor/nvim/lua/configs/conform.lua
@@ -1,15 +1,17 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+	lsp_fallback = true,
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		typescript = { { "prettierd", "prettier" } },
+		typescriptreact = { { "prettierd", "prettier" } },
+		javascript = { { "prettierd", "prettier" } },
+		javascriptreact = { { "prettierd", "prettier" } },
+		json = { { "prettierd", "prettier" } },
+		html = { { "prettierd", "prettier" } },
+		css = { { "prettierd", "prettier" } },
+		astro = { { "prettierd", "prettier" } },
+	},
 }
 
 require("conform").setup(options)
