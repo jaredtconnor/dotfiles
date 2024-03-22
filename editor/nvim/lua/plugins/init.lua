7df0cfeec58d33391e4a2c89025ff1a62dfebf82
Jared Connor <jaredconnor@fastmail.com>
2024-03-21 21:47:41 -0700

    Updating vim doc string

--- editor/nvim/lua/plugins/init.lua
+++ editor/nvim/lua/plugins/init.lua
@@ -29,7 +29,7 @@ return {
   	"nvim-treesitter/nvim-treesitter",
   	opts = {
   		ensure_installed = {
-  			"vim", "lua", "vimdoc
+  			"vim", "lua", "vimdoc",
        "html", "css"
   		},
   	},
