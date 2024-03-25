2689f737047c0fb1759617c1e001c3e758ee1e39
Jared Connor <jaredconnor@fastmail.com>
2024-03-25 07:09:58 -0700

    Moving to full nvim ide

--- editor/nvim/lua/highlights.lua
+++ editor/nvim/lua/highlights.lua
@@ -1,21 +1,38 @@
-local M = {}
--- local colors = require("base46").get_theme_tb "base_30"
+-- NOTE: NvChad Related Highlights
+-- To find any highlight groups: "<cmd> Telescope highlights"
+-- Each highlight group can take a table with variables fg, bg, bold, italic, etc
+-- base30 variable names can also be used as colors
 
+local M = {}
+
+---@type Base46HLGroupsList
 M.override = {
-	Comment = {
-		italic = true,
-	},
-	-- Search = { fg = "black", bg = "blue" },
-	-- IncSearch = { fg = "black", bg = "red" },
-	-- CurSearch = { fg = "black", bg = "blue" },
-	-- Substitute = { fg = "black", bg = "green" },
-	NvDashAscii = { bg = "NONE", fg = "yellow" },
-	NvDashButtons = { bg = "NONE", fg = "red" },
-	FoldColumn = { bg = "NONE" },
-	LspReferenceRead = { link = "Underlined" },
-	LspReferenceText = { link = "Underlined" },
-	LspReferenceWrite = { link = "Underlined" },
-	TblineFill = { link = "Normal" },
+  Comment = {
+    italic = true,
+  },
+  Search = { fg = "black", bg = "blue" },
+  IncSearch = { fg = "black", bg = "red" },
+  CurSearch = { fg = "black", bg = "blue" },
+  Substitute = { fg = "black", bg = "green" },
+  NvDashAscii = { bg = "NONE", fg = "blue" },
+  NvDashButtons = { bg = "NONE" },
+  FoldColumn = { bg = "NONE" },
+  LspReferenceRead = { link = "Underlined" },
+  LspReferenceText = { link = "Underlined" },
+  LspReferenceWrite = { link = "Underlined" },
+  TblineFill = { link = "Normal" },
+}
+
+---@type HLTable
+M.add = {
+  WinBar = { link = "Normal" },
+  WinBarNC = { link = "Normal" },
+  NvimTreeOpenedFolderName = { fg = "green", bold = false },
+  DiffviewDiffAdd = { fg = "black", bg = "green" },
+  DiffviewDiffChange = { fg = "black", bg = "light_grey" },
+  DiffviewDiffModified = { fg = "black", bg = "orange" },
+  DiffviewDiffDelete = { fg = "black", bg = "red" },
+  DiffviewDiffAddAsDelete = { fg = "black", bg = "red" },
 }
 
 return M
