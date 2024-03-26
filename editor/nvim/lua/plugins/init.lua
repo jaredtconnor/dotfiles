febf1f0d9c64a572567b8bf97dbb7f2a514c2d48
Jared Connor <jaredconnor@fastmail.com>
2024-03-25 17:49:55 -0700

    Working lazygit and noice setup

--- editor/nvim/lua/plugins/init.lua
+++ editor/nvim/lua/plugins/init.lua
@@ -1,4 +1,37 @@
 return {
+
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = require "configs.noice",
+
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+  },
+
+  {
+    {
+      "kdheepak/lazygit.nvim",
+      cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+      },
+      -- optional for floating window border decoration
+      dependencies = {
+        "nvim-telescope/telescope.nvim",
+      },
+    },
+  },
+
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre' -- uncomment for format on save
@@ -7,32 +40,33 @@ return {
     end,
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  -- Override plugin definition options
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = require "configs.treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "windwp/nvim-ts-autotag",
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = require "configs.mason",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "nvchad.configs.lspconfig"
+      require "configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  -- Better code action UI
+  {
+    "nvimdev/lspsaga.nvim",
+    event = "LspAttach",
+    config = function()
+      require("lspsaga").setup {}
+    end,
+  },
 }
