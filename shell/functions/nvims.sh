69a35f31af26b2745844d463b99675d9e6e9c455
Jared Connor <jaredconnor301@gmail.com>
2024-04-09 07:48:32 -0700

    Update to nvims

--- shell/functions/nvims.sh
+++ shell/functions/nvims.sh
@@ -1,5 +1,5 @@
 function nvims() {
-	items=("default" "NvChad" "LazyVim")
+	items=("default" "NvChad" "LazyVim" "AstroNvim")
 	config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
 	if [[ -z $config ]]; then
 		echo "Nothing selected"
