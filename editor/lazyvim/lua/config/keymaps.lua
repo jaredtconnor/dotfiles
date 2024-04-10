5f6d5ce7b681ee415a3966cc28f2c4867fba56fd
Jared Connor <jaredconnor@fastmail.com>
2024-04-09 21:04:48 -0700

    Adding new keymaps

--- editor/lazyvim/lua/config/keymaps.lua
+++ editor/lazyvim/lua/config/keymaps.lua
@@ -1,9 +1,4 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local map = vim.keymap.set
 
-local map = vim.keymaps.set
-
---- tab
-map("n", "<S-h>", "<cmd>tabprevious<CR>", "goto next tab")
-map("n", "<S-l>", "<cmd>tabnext<CR> ", "goto prev tab")
+map("n", "<S-h>", "<cmd>tabprevious<CR>", { desc = "go to new tab" })
+map("n", "<S-l>", "<cmd>tabnext<CR> ", { desc = "goto prev tab" })
