f8918e261b1b87f4eaee59732192ea3f3c67b415
Jared Connor <jaredconnor@fastmail.com>
2024-06-07 06:15:32 -0700

    Adding conform setup

--- editor/nvim/lua/plugins/lsp/conform.lua
+++ editor/nvim/lua/plugins/lsp/conform.lua
@@ -3,7 +3,6 @@ local formatters = {
   python = { "ruff" },
   cpp = { "clang_format" },
   c = { "clang_format" },
-  c_sharp = { "clang_format" },
   go = { "gofumpt", "goimports" },
   yaml = { "yamlfmt" },
 }
@@ -27,13 +26,3 @@ local prettier_ft = {
 for _, filetype in pairs(prettier_ft) do
   formatters[filetype] = { "prettier" }
 end
-
-return {
-  format_on_save = {
-    --   -- These options will be passed to conform.format()
-    async = true,
-    quiet = true,
-    lsp_fallback = true,
-  },
-  formatters_by_ft = formatters,
-}
